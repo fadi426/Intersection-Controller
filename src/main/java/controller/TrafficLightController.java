3dceafb9843da12755b0cf1ae02102c5887dbe10
fadi426 <fadiqadadi@hotmail.com>
2019-04-04 22:35:25 +0200

    ChangeLight logic adjustments
    - ChangeLight has been removed and has been refactored to timerTasks for
    red/green/yellow light.
    
    - Timer scheduler has been adjusted
    
    - Controll multiple trafficLights functionallity added
    
    - Array of arrays with groupvalues added

--- src/main/java/controller/TrafficLightController.java
+++ src/main/java/controller/TrafficLightController.java
@@ -8,85 +8,75 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.*;
 
 public class TrafficLightController extends Thread {
-    private List<String> sensorList = new ArrayList<>(Arrays.asList("0", "0"));
+    List<TrafficSensor> greenLightArr = new ArrayList<>();
+    List<TrafficSensor> redLightArr = new ArrayList<>();
+    List<List<String>> groups = new ArrayList<>();
     private TrafficSensorController trafficSensorController;
     private MqttClient mqttClient;
     private String mainTopic;
 
     private Timer timer = new Timer();
-    private int switchTime = 3000;
-    TrafficSensor lastGreen = null;
+    private int switchTime = 5000;
 
     public void run(){
-        timer.schedule(task,0,switchTime);
-//        timer.schedule(task2, 0, switchTime+1000);
+        trafficRegulator();
+        timer.schedule(redLightScheduler,3000,switchTime);
+        timer.schedule(greenLightScheduler,0,switchTime);
+
+//        try {
+//            Thread.sleep(switchTime);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        timer.schedule(task2, 0, switchTime);
     }
 
-    TimerTask task = new TimerTask() {
+    TimerTask greenLightScheduler = new TimerTask() {
         @Override
         public void run() {
-            try {
-                changeLights(mainTopic);
-            } catch (InterruptedException e) {
-                System.out.println("Failed to send message");
+            if (trafficSensorController == null){
+                return;
+            }
+            List<TrafficSensor> trafficSensorList = trafficSensorController.getTrafficSensorList();
+
+            if (trafficSensorList.size() > 0 ) {
+                for (TrafficSensor sensor : trafficSensorList) {
+                    if (sensor.getState().equals("1"))
+                        greenLightArr.add(sensor);
+                }
+                if (greenLightArr.size() > 0) {
+                    Random randomGenerator = new Random();
+                    int randomInt = randomGenerator.nextInt(groups.size());
+                    List<String> randomGroup = groups.get(randomInt);
+
+                    for (TrafficSensor sensor : greenLightArr) {
+                        if (randomGroup.contains(sensor.getGroupId())) {
+                            String publishMsg = mainTopic + "/" + sensor.getGroup() + "/" + sensor.getGroupId() + "/" + "light/" + sensor.getId();
+                            publishMessage(publishMsg, "2");
+                            redLightArr.add(sensor);
+                        }
+                    }
+                }
+            }
+            greenLightArr.clear();
+        }
+    };
+
+    TimerTask task2 = new TimerTask() {
+        @Override
+        public void run() {
+            if (greenLightArr.size() > 0) {
+                for (TrafficSensor sensor : greenLightArr) {
+                    String publishMsg = mainTopic + "/" + sensor.getGroup() + "/" + sensor.getGroupId() + "/" + "light/" + sensor.getId();
+                    publishMessage(publishMsg, "1");
+                }
             }
         }
     };
 
-//    TimerTask task2 = new TimerTask() {
-//        @Override
-//        public void run() {
-//            if (greenLightArr.size() > 0) {
-//                for (TrafficSensor sensor : greenLightArr) {
-//                    String publishMsg = mainTopic + "/" + sensor.getGroup() + "/" + sensor.getGroupId() + "/" + "light/" + sensor.getId();
-//                    publishMessage(publishMsg, "1");
-//                }
-//            }
-//        }
-//    };
-
-
-    public void changeLights(String mainTopic) throws InterruptedException {
-
-        if (trafficSensorController == null){
-            return;
-        }
-        List<TrafficSensor> trafficSensorList = trafficSensorController.getTrafficSensorList();
-        List<TrafficSensor> greenLightArr = new ArrayList<>();
-        List<TrafficSensor> redLightArr = new ArrayList<>();
-
-        if (trafficSensorList.size() > 0 ) {
-            for (TrafficSensor sensor : trafficSensorList){
-                if (sensor.getState().equals("1")) {
-                    greenLightArr.add(sensor);
-                }
-                else if (sensor.getState().equals("0")) {
-                    redLightArr.add(sensor);
-                }
-            }
-            if (lastGreen != null){
-                Thread.sleep(500);
-                String publishMsg = mainTopic + "/" + lastGreen.getGroup() + "/" + lastGreen.getGroupId() + "/" + "light/" + lastGreen.getId();
-                publishMessage(publishMsg, "1");
-                lastGreen = null;
-            }
-            if (greenLightArr.size() > 0) {
-                Random randomGenerator = new Random();
-                int randomInt = randomGenerator.nextInt(greenLightArr.size()) + 1;
-
-                TrafficSensor sensor = greenLightArr.get(randomInt - 1);
-                String publishMsg = mainTopic + "/" + sensor.getGroup() + "/" + sensor.getGroupId() + "/" + "light/" + sensor.getId();
-                publishMessage(publishMsg, "2");
-                lastGreen = sensor;
-                for (TrafficSensor s : greenLightArr) {
-                    if (s != sensor)
-                        redLightArr.add(s);
-                }
-                for (TrafficSensor s : redLightArr){
-                    if (greenLightArr.contains(s))
-                        greenLightArr.remove(s);
-                }
-            }
+    TimerTask redLightScheduler = new TimerTask() {
+        @Override
+        public void run() {
             if (redLightArr.size() > 0) {
                 for (TrafficSensor sensor : redLightArr) {
                     String publishMsg = mainTopic + "/" + sensor.getGroup() + "/" + sensor.getGroupId() + "/" + "light/" + sensor.getId();
@@ -94,16 +84,15 @@ public class TrafficLightController extends Thread {
                 }
             }
             redLightArr.clear();
-            greenLightArr.clear();
         }
-    }
+    };
 
     public void publishMessage(String topic, String content){
         MqttMessage message = new MqttMessage(content.getBytes());
         message.setQos(1);
         try {
             mqttClient.publish(topic, message);
-            System.out.println("Published message: " + content + " Published topic = " + topic);
+            //System.out.println("Published message: " + content + " Published topic = " + topic);
         } catch (MqttException e) {
             System.out.println("Failed to publish message");
         }
@@ -113,5 +102,27 @@ public class TrafficLightController extends Thread {
         this.trafficSensorController = trafficSensorController;
         this.mqttClient = mqttClient;
         this.mainTopic = mainTopic;
+    }
+
+    public void trafficRegulator(){
+        List<String> blue = new ArrayList<>(Arrays.asList("10", "5", "4", "9"));
+        List<String> green = new ArrayList<>(Arrays.asList("10", "1", "4"));
+        List<String> brown = new ArrayList<>(Arrays.asList("11", "9", "1", "7")); //6 ontbreekt hier, maar kan niet tegelijkertijd met 11
+        List<String> purpleLight = new ArrayList<>(Arrays.asList("9", "5", "7"));
+        List<String> pink = new ArrayList<>(Arrays.asList( "1", "2", "3", "4"));; //9 ontbreekt hier, maar kan niet tegelijkertijd met 2
+        List<String> purple = new ArrayList<>(Arrays.asList("9", "7", "8"));    // 11 ontbreekt hier, maar kan niet tegelijkertijd met 8
+        List<String> orange = new ArrayList<>(Arrays.asList("1", "2", "3", "4")); // 7 ontbreekt hier, maar kan niet tegelijkertijd met 3
+        List<String> grey = new ArrayList<>(Arrays.asList( "4", "6", "7")); //1 ontbreekt hier, maar kan niet tegelijkertijd met 5
+
+        groups.add(blue);
+        groups.add(green);
+        groups.add(brown);
+        groups.add(purpleLight);
+        groups.add(pink);
+        groups.add(purple);
+        groups.add(orange);
+        groups.add(grey);
+
+
     }
 }
--- src/main/java/controller/TrafficSensorController.java
+++ src/main/java/controller/TrafficSensorController.java
@@ -9,7 +9,6 @@ public class TrafficSensorController {
 
     private List<TrafficSensor> trafficSensorList = new ArrayList<TrafficSensor>();
     private List<TrafficSensor> tempTrafficSensorList = new ArrayList<TrafficSensor>();
-    private List<TrafficLight> trafficLightsList = new ArrayList<TrafficLight>();
 
     public void changeTrafficSensor(String group, String groupId, String sensorId, String sensorValue) {
         boolean exists = false;
@@ -26,36 +25,12 @@ public class TrafficSensorController {
             }
             else{
                 tempTrafficSensorList.add(new TrafficSensor(group, groupId, sensorId, sensorValue));
-                System.out.println("????????????????????????????????");
             }
             trafficSensorList.addAll(tempTrafficSensorList);
             tempTrafficSensorList.clear();
-            exists = false;
     }
     public List<TrafficSensor> getTrafficSensorList() {
         return trafficSensorList;
-    }
-
-    public List<TrafficSensor> getListFromIterator(ListIterator<TrafficSensor> iterator)
-    {
-        List<TrafficSensor> list = new ArrayList<>();
-
-        // Create an empty list
-        //ArrayList<String> names = new ArrayList<String>( Arrays.asList("alex", "brian", "charles") );
-//        Iterator<Integer> iteraator = Arrays.asList(1, 2, 3, 4, 5).listIterator();
-        // Add each element of iterator to the List
-//        while(iterator.hasNext()) {
-//            //list.add(iterator.previous());
-//            System.out.println(iterator.next() + "<------------------------");
-//        }
-        //List<TrafficSensor> iterable  = (List<TrafficSensor>) iterator;
-//        List<TrafficSensor> mutableList = StreamSupport
-//                .stream(iterable.spliterator(), false)
-//                .collect(Collectors.toList());
-        //iteraator.forEachRemaining(list::add);
-
-        // Return the List
-        return list;
     }
 
 }
